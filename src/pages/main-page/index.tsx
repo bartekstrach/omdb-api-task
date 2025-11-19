83155615afade43568031f6631cf1ffb3866635c
Bartek Strach <iambartekstrach@gmail.com>
2025-11-19 21:34:20 +0100

    chore: extract list item component

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 
-import { FunnelIcon, HeartIcon, InformationCircleIcon, MagnifyingGlassIcon, StarIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { FunnelIcon, MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 import { fetchMovies } from '../../api/omdb';
-import { SearchMoviesResponse } from '../../api/omdb.types';
+import { MovieListItem } from '../../components/movie-list-item';
+import { SearchMoviesResponse } from '../../types/omdb.types';
 
 export const MainPage = () => {
     const [title, setTitle] = useState<string>('');
@@ -65,24 +66,7 @@ export const MainPage = () => {
             </div>
             <div className='flex flex-col space-y-2'>
                 {searchMovieResponse?.Search?.map((movieInfo) => (
-                    <div className='grid grid-cols-[auto_90px_1fr_auto_auto_auto] border border-gray-700 min-h-[100px] items-center hover:bg-gray-100 space-x-4'>
-                        <button className='ml-4 hover:bg-gray-100 rounded transition-colors cursor-pointer' aria-label='Add to favorites'>
-                            <HeartIcon className="hover:fill-current h-6 w-6 text-gray-900" />
-                        </button>
-                        <img src={movieInfo.Poster} className='max-h-[100px]' />
-                        <div className='min-w-0 flex flex-col'>
-                            <h3 className='font-semibold truncate'>{movieInfo.Title}</h3>
-                            <span>{movieInfo.Year}</span>
-                        </div>
-                        <div className='flex items-center space-x-1'>
-                            <StarIcon className="h-6 w-6 text-gray-900" />
-                            <span className='font-semibold'>7.0</span>
-                            <span>(245k)</span>
-                        </div>
-                        <button className="mr-4 hover:bg-gray-100 rounded transition-colors cursor-pointer" aria-label='More info'>
-                            <InformationCircleIcon className="h-6 w-6 text-gray-900" />
-                        </button>
-                    </div>
+                    <MovieListItem movieInfo={movieInfo} />
                 ))}
             </div>
             <p>Pagination</p>
