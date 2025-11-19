e0c40d1a2f047c4d328dd72d08d36417d0dfb4eb
Bartek Strach <iambartekstrach@gmail.com>
2025-11-19 18:13:58 +0100

    feat: add basic api

---
+++ src/api/omdb.ts
@@ -0,0 +1,36 @@
+import {
+    MovieDetails,
+    SearchByRequestParams,
+    SearchMoviesRequestParams,
+    SearchMoviesResponse,
+} from './omdb.types';
+import { queryBuilder } from './utils';
+
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+
+export const fetchMovies = async (
+    params: SearchMoviesRequestParams
+): Promise<SearchMoviesResponse> => {
+    const queryParams = queryBuilder(params);
+
+    const res = await fetch(`${BASE_URL}&${queryParams}`);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch movies');
+    }
+
+    return res.json();
+};
+
+export const fetchMovieDetails = async (params: SearchByRequestParams): Promise<MovieDetails> => {
+    const queryParams = queryBuilder(params);
+
+    const res = await fetch(`${BASE_URL}&${queryParams}`);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch movie details');
+    }
+
+    return res.json();
+};
