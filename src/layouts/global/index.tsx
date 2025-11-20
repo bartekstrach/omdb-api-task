9bc9b7fb2a12b924cb64c31ba3c4c60aa9fc5b8e
Bartek Strach <iambartekstrach@gmail.com>
2025-11-20 16:07:42 +0100

    chore: change gradient

--- src/layouts/global/index.tsx
+++ src/layouts/global/index.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from 'react-router';
 
 export const GlobalLayout = () => (
     <>
-        <header className="flex items-center justify-between p-8 border-b border-gray-700 bg-linear-120 from-teal-500 to-teal-100">
+        <header className="flex items-center justify-between p-8 border-b border-gray-700 bg-gradient-to-br from-teal-500 to-teal-100">
             <Link aria-label="Go to main page" className="flex space-x-4" to="/">
                 <VideoCameraIcon className="h-9 w-9 text-gray-900" />
                 <h1 className="text-3xl font-bold">OMDb API task</h1>
