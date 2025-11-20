21a11ff52483a3064becbb73e71cc7e80335cfc5
Bartek Strach <iambartekstrach@gmail.com>
2025-11-20 22:38:26 +0100

    chore: remove unused code

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,13 +1,10 @@
 import { useState } from 'react';
 
-import { FunnelIcon } from '@heroicons/react/24/outline';
-
 import { MovieList, SearchBar } from '../../components';
 import { useFavorites } from '../../hooks';
 
 export const FavoritesPage = () => {
     const [searchBox, setSearchBox] = useState<string>('');
-    // const [favorites, isLoading] = useFavorites();
     const { favorites, isLoading } = useFavorites();
 
     const filteredFavorites = favorites.filter(movie =>
@@ -18,13 +15,6 @@ export const FavoritesPage = () => {
         <div className="flex flex-col space-y-4">
             <SearchBar isLoading={isLoading} onChange={setSearchBox} value={searchBox} />
             <div className="flex items-center justify-between">
-                <button
-                    className="flex items-center space-x-2 bg-teal-100 hover:bg-teal-500 px-4 py-2 cursor-pointer"
-                    // onClick={searchMovies}
-                >
-                    <FunnelIcon className="h-6 w-6 text-gray-900" />
-                    <span>Filter</span>
-                </button>
                 {filteredFavorites.length && <span>Found {filteredFavorites.length} records</span>}
             </div>
             <MovieList movies={filteredFavorites} />
