cf0f6dc9451cbec75294abbcda4d8d31a4549c48
Bartek Strach <iambartekstrach@gmail.com>
2025-11-21 14:23:58 +0100

    feat: pagination mobile

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const Pagination = ({ currentPage, totalPages, onPageChange }: Props) => {
     return (
-        <div className="flex justify-center items-center gap-8">
+        <div className="flex justify-center items-center gap-2 sm:gap-4 md:gap-8 text-xs sm:text-sm md:text-base">
             <button
                 className="bg-teal-100 hover:bg-teal-500 px-4 py-2 cursor-pointer"
                 disabled={currentPage <= 1}
