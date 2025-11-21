8e276e8113034bc0ff262aeea6c6dfe9f6a9f970
Bartek Strach <iambartekstrach@gmail.com>
2025-11-21 15:37:25 +0100

    feat: vitest setup

---
+++ src/test/test-utils.tsx
@@ -0,0 +1,19 @@
+import { ReactElement, ReactNode } from 'react';
+
+import { render, RenderOptions } from '@testing-library/react';
+
+interface AllProvidersProps {
+    children: ReactNode;
+}
+
+const AllProviders = ({ children }: AllProvidersProps) => {
+    return <>{children}</>;
+};
+
+const customRender = (
+    ui: ReactElement,
+    options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
