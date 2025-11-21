9a99d7fff03e259b25788d7c3ccd33ac28914749
Bartek Strach <iambartekstrach@gmail.com>
2025-11-21 14:33:41 +0100

    feat: center details

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -6,6 +6,6 @@ interface Props {
 export const Message = ({ details, title }: Props) => (
     <div className="flex items-center flex-col gap-4">
         <h2 className="font-bold text-3xl">{title}</h2>
-        <p>{details}</p>
+        <p className="text-center">{details}</p>
     </div>
 );
