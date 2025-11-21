f148bcff9b2ded835f4305ea6c31a8567ad58493
Bartek Strach <iambartekstrach@gmail.com>
2025-11-21 10:36:51 +0100

    feat: filter out null

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,11 @@
 export const queryBuilder = (
-    params: Record<string, string | number | boolean | undefined>
+    params: Record<string, string | number | boolean | undefined | null>
 ): string => {
     const search = new URLSearchParams();
 
     Object.entries(params).forEach(([key, value]) => {
-        if (value !== undefined && value !== '') {
-            // TODO: what to do with params without any value
+        // Ignore undefined, null and empty strings
+        if (value !== undefined && value !== '' && value !== null) {
             search.append(key, String(value));
         }
     });
