7106263906064dd622d88fea547dcc07b036b930
Bartek Strach <iambartekstrach@gmail.com>
2025-11-21 14:36:35 +0100

    feat: header mobile

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,10 +3,14 @@ import { Link, Outlet } from 'react-router';
 
 export const GlobalLayout = () => (
     <>
-        <header className="flex items-center justify-between p-8 border-b border-gray-700 bg-gradient-to-br from-teal-500 to-teal-100">
-            <Link aria-label="Go to main page" className="flex space-x-4" to="/">
-                <VideoCameraIcon className="h-9 w-9 text-gray-900" />
-                <h1 className="text-3xl font-bold">OMDb API task</h1>
+        <header className="flex items-center justify-between gap-4 p-8 border-b border-gray-700 bg-gradient-to-br from-teal-500 to-teal-100">
+            <Link aria-label="Go to main page" className="flex items-center space-x-4" to="/">
+                <div className="hidden sm:flex">
+                    <VideoCameraIcon className="h-9 w-9 text-gray-900" />
+                </div>
+                <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold">
+                    OMDb API task
+                </h1>
             </Link>
             <Link aria-label="View favorites" to="/favorites">
                 <HeartIcon className="hover:fill-current h-9 w-9 text-gray-900" />
